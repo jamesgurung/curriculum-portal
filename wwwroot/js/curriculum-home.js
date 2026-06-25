44c97b8042fb10e4d65fb3909326dd13e76763f8
jamesgurung <jamesgurung@users.noreply.github.com>
2026-06-25 10:38:16 +0100

    Allow case insensitive file extensions

--- wwwroot/js/curriculum-home.js
+++ wwwroot/js/curriculum-home.js
@@ -1435,7 +1435,7 @@ function validateDocumentUrl(url) {
     return false;
   }
 
-  if (!url.includes('.docx?') && !url.includes('.xlsx?') && !url.includes('.pdf?')) {
+  if (!url.toLowerCase().includes('.docx?') && !url.toLowerCase().includes('.xlsx?') && !url.toLowerCase().includes('.pdf?')) {
     alert('Only Microsoft Word or Excel documents are accepted. Please copy the link to a valid .docx or .xlsx file.');
     return false;
   }
